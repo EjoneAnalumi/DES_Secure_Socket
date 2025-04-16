90546cdf48cd07b88dd154c78fec42e7085d8605
edonitagashi <edonita.gashi@student.uni-pr.edu>
2025-04-16 19:53:30 +0200

    Update Client.java
    Krijimi i lidhjes me server dhe marrja e input-it

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,19 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 5001;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
+            System.out.print("Shkruaj një mesazh për të dërguar në server: ");
+            String message = scanner.nextLine();
+
+        } catch (IOException e) {
+            System.out.println("Gabim gjatë lidhjes me serverin: " + e.getMessage());
+        }
+    }
 }
