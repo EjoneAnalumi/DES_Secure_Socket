2defdb0bb81c8985db99f7a0a3b3bf91b50c1ce6
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-16 20:26:05 +0200

    inicimi i serverit me algoritmin DES dhe celesin sekret

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,18 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-}
+    private static final String SECRET_KEY = "12345678";
+    private static final String ALGORITHM = "DES";
+
+    public static void main(String[] args) {
+        int port = 5000;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("🔐 Serveri eshte duke pritur ne portin " + port + "...");
+
+            Socket socket = serverSocket.accept();
+            System.out.println("✅ Klienti u lidh: " + socket.getInetAddress());
+
+
+        }
